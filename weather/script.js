4bf276b1ad65bec7d9d235e7608c43eb881a290c
unknown <ramovic225@gmail.com>
2025-02-21 21:59:00 +0100

    error message added

--- weather/script.js
+++ weather/script.js
@@ -2,60 +2,60 @@ const apiKey = "2e91349e4b8e48e395575630252002";
 const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}`;
 
 async function fetchWeather(searchedCity) {
-  try {
-    const response = await fetch(apiUrl + `&q=${searchedCity}`);
-    var data = await response.json();
+  const response = await fetch(apiUrl + `&q=${searchedCity}`);
+  var data = await response.json();
 
-    function changeDateFormat(formatedDate) {
-      const date = formatedDate.split(" ");
-      const dateArray = date[0].split("-").reverse().join(".");
+  if (response.status === 400) {
+    document.querySelector(".error-message").style.display = "block";
+    document.querySelector(".weather-data").style.display = "none";
+  }
 
-      return dateArray + " " + date[1];
-    }
+  if (searchedCity && response.status !== 400) {
+    document.querySelector(".weather-data").style.display = "flex";
+    document.querySelector(".error-message").style.display = "none";
+  }
 
-    document.querySelector("#condition-text").innerHTML =
-      data.current?.condition?.text;
-    document.querySelector("#city-name").innerHTML =
-      data?.location?.name + ", " + data.location.country;
-    document.querySelector("#date").innerHTML = changeDateFormat(
-      data.current.last_updated
-    );
-    document.querySelector("#weather-temperature").innerHTML =
-      Math.round(data.current.temp_c) + "°";
-    document.querySelector("#humidity").innerHTML = data.current.humidity + "%";
-    document.querySelector("#wind").innerHTML = data.current.wind_kph + "km/h";
-    document.querySelector("#dew-point").innerHTML =
-      data.current.dewpoint_c + "°";
-    document.querySelector("#pressure").innerHTML =
-      data.current.pressure_mb + "hPa";
+  function changeDateFormat(formatedDate) {
+    const date = formatedDate.split(" ");
+    const dateArray = date[0].split("-").reverse().join(".");
 
-    const weatherType = document.querySelector("#weather-type");
+    return dateArray + " " + date[1];
+  }
 
-    let existingIcon = weatherType.querySelector("img");
+  document.querySelector("#condition-text").innerHTML =
+    data.current?.condition?.text;
+  document.querySelector("#city-name").innerHTML =
+    data?.location?.name + ", " + data.location.country;
+  document.querySelector("#date").innerHTML = changeDateFormat(
+    data.current.last_updated
+  );
+  document.querySelector("#weather-temperature").innerHTML =
+    Math.round(data.current.temp_c) + "°";
+  document.querySelector("#humidity").innerHTML = data.current.humidity + "%";
+  document.querySelector("#wind").innerHTML = data.current.wind_kph + "km/h";
+  document.querySelector("#dew-point").innerHTML =
+    data.current.dewpoint_c + "°";
+  document.querySelector("#pressure").innerHTML =
+    data.current.pressure_mb + "hPa";
 
-    if (existingIcon) {
-      existingIcon.src = `https:${data.current?.condition?.icon}`;
-      existingIcon.alt = data.current?.condition?.text;
-    } else {
-      const icon = document.createElement("img");
-      icon.src = `https:${data.current?.condition?.icon}`;
-      icon.alt = data.current?.condition?.text;
-      weatherType.appendChild(icon);
-    }
+  const weatherType = document.querySelector("#weather-type");
 
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-    alert("Failed to fetch weather data");
+  let existingIcon = weatherType.querySelector("img");
+
+  if (existingIcon) {
+    existingIcon.src = `https:${data.current?.condition?.icon}`;
+    existingIcon.alt = data.current?.condition?.text;
+  } else {
+    const icon = document.createElement("img");
+    icon.src = `https:${data.current?.condition?.icon}`;
+    icon.alt = data.current?.condition?.text;
+    weatherType.appendChild(icon);
   }
 }
 const searchInput = document.querySelector(".search input");
 const searchButton = document.querySelector(".search button");
 
 searchButton.addEventListener("click", () => {
-  if (searchInput.value.trim() === "") {
-    alert("Type a city name");
-  }
   fetchWeather(searchInput.value);
   searchInput.value = "";
 });
