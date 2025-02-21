dc8e3f9d10f6ed09f082e02c5b50bac173f88314
unknown <ramovic225@gmail.com>
2025-02-21 18:40:58 +0100

    changeDateFormat function added

--- weather/script.js
+++ weather/script.js
@@ -5,10 +5,19 @@ async function fetchWeather() {
   const response = await fetch(apiUrl + `&q=Belgrade`);
   var data = await response.json();
 
+  function changeDateFormat(formatedDate) {
+    const date = formatedDate.split(" ");
+    const dateArray = date[0].split("-").reverse().join(".");
+
+    return dateArray + " " + date[1];
+  }
+
   document.querySelector("#condition-text").innerHTML =
     data.current?.condition?.text;
   document.querySelector("#city-name").innerHTML = data?.location?.name;
-  document.querySelector("#date").innerHTML = data.current.last_updated;
+  document.querySelector("#date").innerHTML = changeDateFormat(
+    data.current.last_updated
+  );
   document.querySelector("#humidity").innerHTML = data.current.humidity;
   document.querySelector("#wind").innerHTML = data.current.wind_kph;
   document.querySelector("#dew-point").innerHTML = data.current.dewpoint_c;
