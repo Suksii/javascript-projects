160ac057398ddc673be12d30a482f46be9fcf921
unknown <ramovic225@gmail.com>
2025-02-25 20:47:28 +0100

    function errors fixed

--- ageCalculator/script.js
+++ ageCalculator/script.js
@@ -16,23 +16,23 @@ function ageCalculator() {
   const selectedMonth = Number(selectedDate.split("-")[1]);
   const selectedYear = Number(selectedDate.split("-")[0]);
 
-  let newDay;
-  let newMonth;
-  let newYear;
+  let newDay = currentDay - selectedDay;
+  let newMonth = currentMonth - selectedMonth;
+  let newYear = currentYear - selectedYear;
 
-  newYear = currentYear - selectedYear;
-
-  if (currentMonth >= selectedMonth) {
-    newMonth = currentMonth - selectedMonth;
-  } else {
-    newMonth = 12 + (currentMonth - selectedMonth);
-    newYear -= 1;
+  if (new Date(selectedYear, selectedMonth - 1, selectedDay) > currentDate) {
+    alert("Selected date cannot be in the future");
+    return;
   }
-  if (currentDay >= selectedDay) {
-    newDay = currentDay - selectedDay;
-  } else {
-    newDay = 30 + (currentDay - selectedDay);
+
+  if (newDay < 0) {
+    const lastMonth = new Date(currentYear, currentMonth - 1, 0).getDate();
+    newDay += lastMonth;
     newMonth -= 1;
+  }
+  if (newMonth < 0) {
+    newMonth += 12;
+    newYear -= 1;
   }
   console.log(newDay, newMonth, newYear);
   result.innerHTML = `You are <span>${newYear}</span> years, <span>${newMonth}</span> months and <span>${newDay}</span> days old`;
