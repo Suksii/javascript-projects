4659d36ccf34ce54b1bd0a8daa8c8e1cf4243226
unknown <ramovic225@gmail.com>
2025-02-26 16:48:48 +0100

    error message & transition added

--- QRCodeGenerator/script.js
+++ QRCodeGenerator/script.js
@@ -1,10 +1,19 @@
 const inputValue = document.querySelector(".input input");
 const button = document.querySelector(".container button");
 const image = document.querySelector(".qr-img img");
+const error = document.querySelector("#error");
 
 button.addEventListener("click", () => {
+  if (inputValue.value.trim() === "") {
+    error.innerHTML = "Input field is empty";
+    setTimeout(() => {
+      error.innerHTML = "";
+    }, 2000);
+    return;
+  }
   image.src =
     "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" +
     inputValue.value;
   image.alt = inputValue.value;
+  document.querySelector(".qr-img").classList.add("show-img");
 });
